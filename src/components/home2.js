04ff6aff1f4f5691a8cceaf8e5334bb7dffcca86
Brian J. Strout <brianjstrout@gmail.com>
2022-10-19 22:41:17 +0100

    updated components for basic skeleton

---
+++ src/components/home2.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+// import { Link } from "react-router-dom";
+// import { motion } from "framer-motion";
+// import ProgressiveImage from "react-progressive-image";
+
+// const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+export default function Home2() {
+
+
+  return(
+
+        <div className='container'>
+            <h3> home</h3>
+        </div>
+  )
+  };
+
+//  Home2;
---
+++ src/components/introducing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const introducing = () => {
+  return (
+    <div>introducing Masha</div>
+  )
+}
+
+export default introducing
