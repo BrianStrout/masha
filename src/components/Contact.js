d2e8b41b17494e4d8b7bbead20d09d6de6355c42
Brian J. Strout <brianjstrout@gmail.com>
2022-11-10 00:18:34 +0000

    updated contact area and portfolio area

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,35 @@
 import React from "react";
+import { useRef } from "react";
 import "../App.css";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
+import emailjs from "@emailjs/browser";
+
 // import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  // const form = useRef();
+  const form = useRef();
 
-  // const sendEmail = (e) => {
-  //   // e.preventDefault();
+  const sendEmail = (e) => {
+    // e.preventDefault();
 
-  //   emailjs.sendForm('service_vwdp4la', 'template_43yitzb', form.current, 'oYymItkIoREaVvBlM')
-  //   alert("thanks!");
-  // };
+    emailjs.sendForm(
+      "service_vwdp4la",
+      "template_43yitzb",
+      form.current,
+      "oYymItkIoREaVvBlM"
+    );
+    alert("thanks!");
+  };
   return (
     <section id="contact">
       <div className="container contact__container">
+        <div className="contacthi">
+          <h2>contact me</h2>
+          <br />
+          <h5>get in touch</h5>
+        </div>
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail />
@@ -24,6 +37,7 @@ const Contact = () => {
             <h5>email.email.com</h5>
             <a href="mailto:me@me.com">send an email</a>
           </article>
+
           <article className="contact__option">
             <RiMessengerLine />
             <h4>fb/if messenger</h4>
@@ -31,15 +45,27 @@ const Contact = () => {
             <a href="https://m.me/brian.strout">message me</a>
           </article>
         </div>
-        <h2>contact me</h2>
-        {/* <h5>get in touch</h5> */}
 
-        {/* <form ref={form} onSubmit={sendEmail}> 
-<input type="text" name='name' placeholder='Your Full Name' required/>
-<input type="email" name='email' placeholder='you@you.com' required/>
-<textarea name="message" id="message" cols="5" rows="10" required></textarea>
-<button type='submit' className='btn btn-primary'>Send Mesg</button>
-</form> */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="you@you.com" required />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Your kindest message"
+            cols="5"
+            rows="10"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Mesg
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/GalleryCase.js
+++ src/components/GalleryCase.js
@@ -16,42 +16,115 @@ const galleryCase = () => {
   return (
     <div className="GalleryDisplayCase">
       <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Summer1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+      <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Blur1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+      <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Class1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+      <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Orange1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+      <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Pinboard1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+      <div className="DisplayItem">
+        <div
+          className="Display_Item_Photo"
+          style={{
+            backgroundImage: `url(${Techni1})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+
+      {/*
+      <div className="DisplayItem">
         <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Summer1} alt="hi there" />
+          <div
+            className="Merch_Item_Image"
+            style={{ background-image: { Blur1 } }}
+          ></div>
         </div>
         <div className="Display_Title">Summer Shot</div>
       </div>
 
       <div className="DisplayItem">
         <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Blur1} alt="hi there" />
+          <div
+            className="Merch_Item_Image"
+            style={{ background-image: { Class1 } }}
+          ></div>
+        </div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+
+      <div className="DisplayItem">
+        <div className="Display_Item_Photo">
+          <div
+            className="Merch_Item_Image"
+            style={{ background-image: { Orange1 } }}
+          ></div>
+        </div>
+        <div className="Display_Title">Summer Shot</div>
+      </div>
+
+      <div className="DisplayItem">
+        <div className="Display_Item_Photo">
+          <div
+            className="Merch_Item_Image"
+            style={{ background-image: { Pinboard1 } }}
+          ></div>
         </div>
         <div className="Display_Title">Summer Shot</div>
       </div>
       <div className="DisplayItem">
         <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Class1} alt="hi there" />
+          <div
+            className="Merch_Item_Image"
+            style={{ background-image: { Techni1 } }}
+          ></div>
         </div>
         <div className="Display_Title">Summer Shot</div>
-      </div>
-      <div className="DisplayItem">
-        <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Orange1} alt="hi there" />
-        </div>
-        <div className="Display_Title">Summer Shot</div>
-      </div>
-      <div className="DisplayItem">
-        <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Pinboard1} alt="hi there" />
-        </div>
-        <div className="Display_Title">Summer Shot</div>
-      </div>
-      <div className="DisplayItem">
-        <div className="Display_Item_Photo">
-          <img className="Merch_Item_Image" src={Techni1} alt="hi there" />
-        </div>
-        <div className="Display_Title">Summer Shot</div>
-      </div>
+      </div> */}
     </div>
   );
 };
